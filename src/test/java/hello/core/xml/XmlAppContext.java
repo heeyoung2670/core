2eb1892cf1c597af3bb5e8833d295cd834f6b9dd
heeyoung2670 <khyoung2670@naver.com>
2022-03-03 20:58:15 +0900

    xml 기반의 스프링 빈 설정 정보

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,21 @@
+package hello.core.xml;
+
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        //xml 파일에 스프링 설정 정보를 생성 -> 애노테이션 기반 코드와 똑같이 동작
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+}
